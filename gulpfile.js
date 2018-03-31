53aa9b6c878c47cfc762393c9797220c40dd181f
Roman Luntsevich <shipik2008@gmail.com>
2018-03-31 16:01:03 +0300

    Create style

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿/// <binding AfterBuild='scripts' />
+var gulp = require('gulp');
+
+var requireDir = require('require-dir');
+requireDir('./gulp-tasks');
+
+// ------------------------------------------------------------------------
+
+// gulp.task('style', ['css.site']);
+// gulp.task('scripts', ['scripts.core', 'scripts.iframe', 'scripts.admin', 'scripts.website']);
+// gulp.task('svg', ['svg.min', 'svg.inject']);
+
+gulp.task("watch", function () {
+	// gulp.watch(["websrc/less/**/*.less"], ["style"]);
+	gulp.watch(["websrc/less/**/*.less"], ["css.site"]);
+	// gulp.watch(["ClientApp/app/**/*.*"], ["scripts"]);
+	// gulp.watch(["web-src/svg/*.svg"], ["svg"]);
+});
+
